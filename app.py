df2298bc82b90863e245ac13723973d9bdcac532
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2021-08-18 18:31:09 +0100

    added home page user entry

--- app.py
+++ app.py
@@ -59,6 +59,49 @@ def setColourScheme(frame, colour_string):
     label1t = Label(image=imgt)
     label1t.image = imgt
     label1t.place(x=(WINDOW_W//4)*1, y=WINDOW_H//2, anchor="center")
+def createHomePage():
+    global home_frame
+    try:
+        home_frame.destroy()
+        lobby_frame.destroy()
+    except:
+        pass
+    home_frame = tk.Frame(root, bg=COL_PRIME).place(relwidth=1, relheight=1, relx=0, rely=0)
+    
+    image = Image.open("res/images/card_packs/" + COL_SCHEME + "/as.png")
+    image = image.resize((500//3, 726//3))
+    img = ImageTk.PhotoImage(image)
+    label1 = Label(image=img)
+    label1.image = img
+    label1.place(x=(WINDOW_W//4)*3, y=WINDOW_H//2, anchor="center")
+
+    imaget = Image.open("res/images/card_packs/" + COL_SCHEME + "/bk.png")
+    imaget = imaget.resize((500//3, 726//3))
+    imgt = ImageTk.PhotoImage(imaget)
+    label1t = Label(image=imgt)
+    label1t.image = imgt
+    label1t.place(x=(WINDOW_W//4)*1, y=WINDOW_H//2, anchor="center")
+    
+    username_label = Label(home_frame, text="Username:", bg=COL_PRIME, fg=COL_TEXT)
+    username_label.place(x=WINDOW_W//2-60, y=WINDOW_H*0.4, anchor="center")
+
+    username_entry = tk.Entry(home_frame, width=18, bg=COL_WIDGET, fg=COL_TEXT)
+    username_entry.place(x=WINDOW_W//2+40, y=WINDOW_H*0.4, anchor="center")
+    username_entry.insert(0, USERNAME)
+
+    identifier_label = Label(home_frame, text="Identifier:", bg=COL_PRIME, fg=COL_TEXT)
+    identifier_label.place(x=WINDOW_W//2-60, y=WINDOW_H*0.4+30, anchor="center")
+    identifier_entry = tk.Entry(home_frame, width=18, bg=COL_WIDGET, fg=COL_TEXT)
+    identifier_entry.place(x=WINDOW_W//2+40, y=WINDOW_H*0.4+30, anchor="center")
+    identifier_entry.insert(0, IDENTIFIER)
+
+    colour_string = tk.StringVar(home_frame)
+    colour_string.set("Colour Scheme: " + str(COL_SCHEME))
+    colour_option_menu = tk.OptionMenu(home_frame, colour_string, *COL_SCHEMES, command= lambda x=None: setColourScheme(home_frame, colour_string))
+    colour_option_menu.config(width=21, bg=COL_WIDGET, fg=COL_TEXT)
+    colour_option_menu["menu"].config(bg=COL_WIDGET, fg=COL_TEXT)
+    colour_option_menu.place(x=WINDOW_W//2, y=WINDOW_H*0.4+70, anchor="center")
+
 def main():
     createHomePage()
 
