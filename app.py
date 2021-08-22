7264d9632110874bee1bd22e9bc9b48e56ad0324
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2021-08-22 21:24:40 +0100

    added cards deck

--- app.py
+++ app.py
@@ -20,6 +20,13 @@ COL_SCHEMES = ["Default", "test"]
 WINDOW_SIZES    = ["800x600", "900x700", "1200x700", "1600x1100", "2100x1300"]
 COL_SCHEMES     = ["Default", "test"]
 
+CARDS_DECK      = ["2c", "2d", "2h", "2s", "3c", "3d", "3h", "3s",
+                   "4c", "4d", "4h", "4s", "5c", "5d", "5h", "5s",
+                   "6c", "6d", "6h", "6s", "7c", "7d", "7h", "7s",
+                   "8c", "8d", "8h", "8s", "9c", "9d", "9h", "9s",
+                   "vc", "vd", "vh", "vs", "wjc", "wjd", "wjh", "wjs",
+                   "xqc", "xqd", "xqh", "xqs", "ykc", "ykd", "ykh", "yks",
+                   "zac", "zad", "zah", "zas"]
 
 USERNAME    = ""
 IDENTIFIER  = ""
