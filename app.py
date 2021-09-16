fadecd2b3b19c18a7de3e9c4dd82b1f4bd42d980
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2021-09-16 10:46:43 +0100

    added client class initialization

--- app.py
+++ app.py
@@ -12,6 +12,26 @@ from res.classes.UserConfig import UserConfig
 from res.classes.OfflineGame import OfflineGame
 
 
+class contractWhistClient:
+    def __init__(self, parent):
+        self.window = parent
+        self.window.resizable(width=False, height=False)
+        self.window.title = "Contract Whist Client - 1.0"
+        self.window.iconbitmap("res/images/icon.ico")
+        self.window_sizes = ["800x600", "900x700", "1200x700", "1600x1100", "2100x1300"]
+
+        self.colour_schemes = ["Default", "test"]
+
+        self.colour_prime   = "PaleGreen1"
+        self.colour_second  = "PaleGreen2"
+        self.colour_text    = "DarkGreen"
+        self.colour_widget  = "DarkSeaGreen2"
+
+        self.cards_deck = ["2c", "2d", "2h", "2s", "3c", "3d", "3h", "3s", "4c", "4d", "4h", "4s",
+                           "5c", "5d", "5h", "5s", "6c", "6d", "6h", "6s", "7c", "7d", "7h", "7s",
+                           "8c", "8d", "8h", "8s", "9c", "9d", "9h", "9s", "vc", "vd", "vh", "vs",
+                           "wjc", "wjd", "wjh", "wjs", "xqc", "xqd", "xqh", "xqs", "ykc", "ykd",
+                           "ykh", "yks", "zac", "zad", "zah", "zas"]
 
 
 
