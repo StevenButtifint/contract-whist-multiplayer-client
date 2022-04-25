e1d688dcf72597d70319c39ad4659b4a5551d3e9
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2022-04-25 11:49:26 +0100

    added colour constants

--- constants.py
+++ constants.py
@@ -7,3 +7,11 @@ WINDOW_SIZES = ["800x600", "900x700", "1200x700", "1600x1100", "2100x1300"]
 CARD_PACKS_LOC = "res/card_packs"
 
 
+#COLOURS
+COLOUR_PRIME = "PaleGreen1"
+COLOUR_SECOND = "PaleGreen2"
+COLOUR_TEXT = "black"
+COLOUR_WIDGET = "DarkSeaGreen2"
+COLOUR_CENTER = "forest green"
+
+
