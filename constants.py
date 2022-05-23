5ed4dee4974394b16839eec64d60864249360b56
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2022-05-23 20:32:08 +0100

    added cards deck imported constant

--- constants.py
+++ constants.py
@@ -15,3 +15,10 @@ COLOUR_WIDGET = "DarkSeaGreen2"
 COLOUR_CENTER = "forest green"
 
 
+#OFFLINE CONSTANTS
+CARDS_DECK = ["2c", "2d", "2h", "2s", "3c", "3d", "3h", "3s", "4c", "4d", "4h", "4s",
+              "5c", "5d", "5h", "5s", "6c", "6d", "6h", "6s", "7c", "7d", "7h", "7s",
+              "8c", "8d", "8h", "8s", "9c", "9d", "9h", "9s", "vc", "vd", "vh", "vs",
+              "wjc", "wjd", "wjh", "wjs", "xqc", "xqd", "xqh", "xqs", "ykc", "ykd",
+              "ykh", "yks", "zac", "zad", "zah", "zas"]
+
