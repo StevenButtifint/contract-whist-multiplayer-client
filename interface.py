c1385e2346079bbaae5aca9a2dcb40238aaf8292
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2022-07-01 17:13:02 +0100

    added make frame function

--- interface.py
+++ interface.py
@@ -5,3 +5,9 @@ from constants import *
 
 
 
+def makeFrame(frame, bg, rw, rh, rx, ry, anchor):
+    new_frame = tk.Frame(frame, bg=bg)
+    new_frame.place(relwidth=rw, relheight=rh, relx=rx, rely=ry, anchor=anchor)
+    return new_frame
+
+
